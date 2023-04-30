33f751562fbc56fb08e5de1368a32db724c9382b
ttakuma17 <ttanimiya@gmail.com>
2023-04-30 12:03:56 +0900

    リクエストの受付処理の実装

---
+++ src/test/java/com/tax/caluculation/application/controller/HelloWorldControllerTest.java
@@ -0,0 +1,23 @@
+package com.tax.caluculation.application.controller;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+@WebMvcTest(HelloWorldController.class)
+class HelloWorldControllerTest {
+
+  @Autowired
+  private MockMvc mockMvc;
+
+  @Test
+  public void testHelloWorld() throws Exception{
+    mockMvc.perform(get("/"))
+        .andExpect(status().isOk())
+        .andExpect(content().string("Hello World"));
+  }
+}
